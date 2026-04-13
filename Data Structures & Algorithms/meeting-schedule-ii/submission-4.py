f11838ea938acebd56fe821e0eab9a1f9cc675d8
rocketwings <shuni93@vt.edu>
2026-04-12 22:13:17 -0700

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.py
@@ -0,0 +1,59 @@
+import heapq
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        if len(intervals) == 1:
+            return 1
+        # if there is two
+        res = 1
+        intervals.sort(key=lambda x: x.start) #sort by start
+        rooms_heap = [intervals[0].end]
+
+        for i in range(1, len(intervals)):
+            '''
+            if interval.start < prev_end:
+                res += 1
+                prev_end = min(interval.end, prev_end)
+            '''
+            if intervals[i].start >= rooms_heap[0]:
+                heapq.heappop(rooms_heap) #pop the minimum meeting ends
+            heapq.heappush(rooms_heap, intervals[i].end) # push the current meetings ends to the heapq
+
+        return len(rooms_heap)
+
+
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+
+        res = count = 0
+        s = e = 0
+        while s < len(intervals):
+            if start[s] < end[e]: #start less than end
+                s += 1
+                count += 1
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        return res
+
+        
