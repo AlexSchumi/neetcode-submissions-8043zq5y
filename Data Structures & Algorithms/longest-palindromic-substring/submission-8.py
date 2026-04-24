17ed9e617e294e7b0ae75ec1c175192472e8e43a
rocketwings <shuni93@vt.edu>
2026-04-24 16:40:31 -0700

    Add: longest-palindromic-substring - submission-8

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 0:
+            return ''
+
+
+        res = ''
+        n = len(s)
+        for i in range(n):
+            for j in range(i+1, n+1):
+                cur_str = s[i:j]
+                if cur_str == cur_str[::-1] and len(cur_str) > len(res):
+                    res = cur_str
+        return res
