67502b0d90cb57e5244099a66c97345328bb09b0
rocketwings <shuni93@vt.edu>
2026-05-05 01:18:22 -0700

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dicts = {
+            '{': '}',
+            '[': ']',
+            '(': ')'
+        }
+        stacks = []
+        for _s in s:
+            if _s in list(dicts.keys()):
+                stacks.append(dicts[_s])
+            else:
+                if not stacks:
+                    return False
+                para = stacks.pop(-1)
+                if _s != para:
+                    return False
+        return len(stacks) == 0
+
+        
