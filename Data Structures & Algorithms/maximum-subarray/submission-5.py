a3ada0dff9b56d71e87df32005674af6ca5c9a99
rocketwings <shuni93@vt.edu>
2026-05-12 13:54:10 -0700

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.py
@@ -0,0 +1,31 @@
+# brute-force solution
+# runtime: O(N^2)
+# space: O(1)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = nums[0]
+        for i in range(len(nums)):
+            cur_sum = nums[i]
+            res = max(cur_sum, res)
+
+            for j in range(i+1, len(nums)):
+                cur_sum += nums[j]
+                res = max(cur_sum, res)
+        return res
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+        for num in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += num
+            maxSub = max(maxSub, curSum)
+        return maxSub
+
+
+
+
+        
