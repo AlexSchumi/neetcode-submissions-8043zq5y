325b8afa2016265e31fca3e53f16d75c0fc9c606
rocketwings <shuni93@vt.edu>
2026-05-22 02:30:33 -0700

    Update: non-overlapping-intervals - submission-2

--- Data Structures & Algorithms/non-overlapping-intervals/submission-2.py
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-2.py
@@ -1,39 +1,21 @@
 class Solution:
-    #brute force solution
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-
-        def dfs(i, prev):
-            if i == len(intervals):
-                return 0
-            res = dfs(i + 1, prev)
-            if prev == -1 or intervals[prev][1] <= intervals[i][0]:
-                res = max(res, 1 + dfs(i + 1, i))
-            return res
-
-        return len(intervals) - dfs(0, -1)
-    
-    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-        n = len(intervals)
-        if n == 1:
+        if not intervals:
             return 0
+        sorted_intervals = sorted(intervals, key=lambda x: x[0]) # sort start time
+        prev_start, prev_end = sorted_intervals[0][0], sorted_intervals[0][1]
+        res = 0
 
-        cur_end = intervals[0][1]
-        cnt = 0
-        # at least having two
-        for i in range(1, n):
-            # check if next has overlaps
-            if intervals[i][0] < cur_end:
-                cnt += 1
-                #cur_interval = intervals[i]
-                cur_end = min(cur_end, intervals[i][1])
-            else:
-                cur_end = intervals[i][1]
+        for i in range(1, len(intervals)):
+            start = sorted_intervals[i][0]
+            end = sorted_intervals[i][1]
 
-        return cnt
+            if start < prev_end: #overlap
+                res += 1
+                end = min(prev_end, end)
+            prev_end = end
+        return res
 
 
-        
 
         
